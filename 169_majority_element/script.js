84ffdb2b7edaeb70ab7f1cce222f0e529953c9a8
gerrymoeis <gerrymoeis@gmail.com>
2025-07-14 20:40:29 +0700

    169 Majority Element

--- 169_majority_element/script.js
+++ 169_majority_element/script.js
@@ -13,15 +13,16 @@ var majorityElement = function(nums) {
         count = 1
       }
       else if (n === nums[i]) count++
-      else if (n !== nums[i]) count--
+      else count--
     }
     return n
 };
 
 // let nums = [3,2,3]
-// let nums = [3,2,3,4,4,4,4,4,2,2,6,1,2,3,4,1,4,100,234,234,12,7,8,4,1]
-// let nums = [1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-let nums = [2,2,1,1,1,2,2]
+// let nums = [-1, -1, -1, -1, 2, 3, 4]
+// let nums = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let nums = [2,2,1,1,1,2,2]
+let nums = [1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 999999999, 999999998]
 console.log(majorityElement(nums))
 
 // Experiment
