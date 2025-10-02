49eb737c05e90def3e9fd23e4f830a413e03d5be
gerrymoeis <gerrymoeis@gmail.com>
2025-10-02 21:40:56 +0700

    3100

---
+++ 3100_water_bottles_2/script.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var maxBottlesDrunk = function(numBottles, numExchange) {
+    let drunk = numBottles;
+    let empty = numBottles;
+    while (empty >= numExchange) {
+        drunk += 1;
+        empty -= numExchange - 1; 
+        numExchange += 1;
+    }
+    return drunk;
+};
