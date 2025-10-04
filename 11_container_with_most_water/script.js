3abbc03cf46d8c523c412d8ddb6dafdd8d90ca01
gerrymoeis <gerrymoeis@gmail.com>
2025-10-04 18:00:14 +0700

    11

---
+++ 11_container_with_most_water/script.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i = 0;
+    let j = height.length - 1;
+    let res = 0;
+    while (i < j) {
+        res = Math.max(res, (j-i) * Math.min(height[i], height[j]));
+        if (height[i] < height[j]) ++i;
+        else --j;
+    }
+    return res;
+};
