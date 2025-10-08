9eb0423214f05188847fcd8faee766457a798ffb
gerrymoeis <gerrymoeis@gmail.com>
2025-10-08 18:13:56 +0700

    2300

--- 2300_successful_pairs_of_spells_and_potions/script.js
+++ 2300_successful_pairs_of_spells_and_potions/script.js
@@ -5,31 +5,20 @@
  * @return {number[]}
  */
 var successfulPairs = function(spells, potions, success) {
-    const last = new Map();
-    const q = [];
-    const res = [];
-    for (let i = 0; i < rains.length; i++){
-        const lake = rains[i];
-        if (lake !== 0){
-            if (last.has(lake)){
-                let found = false;
-                for (let j = 0; j < q.length; j++){
-                    if (q[j] > last.get(lake)) {
-                        res[q[j]] = lake;
-                        q.splice(j, 1);
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found)
-                return [];
-            }
-            res.push(-1);
-            last.set(lake, i);
-        } else {
-            res.push(1);
-            q.push(i);
+    potions.sort((a, b) => a - b);
+    const n = potions.length;
+    const ans = [];
+
+    for (let spell of spells) {
+        let low = 0, high = n - 1;
+        while (low <= high) {
+            const mid = Math.floor((low + high) / 2);
+            if (BigInt(spell) * BigInt(potions[mid]) >= BigInt(success))
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
+        ans.push(n - low);
     }
-    return res;
+    return ans;
 };
