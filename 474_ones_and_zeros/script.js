d1f89335771049ec004e2c2be5f62fd80cdc9a0b
gerrymoeis <gerrymoeis@gmail.com>
2025-11-11 18:19:23 +0700

    474

---
+++ 474_ones_and_zeros/script.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function(strs, m, n) {
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    for (const s of strs) {
+        let z = 0, o = 0;
+        for (const ch of s) (ch === '0') ? z++ : o++;
+
+        for (let i = m; i >= z; i--) {
+        for (let j = n; j >= o; j--) {
+            dp[i][j] = Math.max(dp[i][j], dp[i - z][j - o] + 1);
+        }
+        }
+    }
+    return dp[m][n];
+};
