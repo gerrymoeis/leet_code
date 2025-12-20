32f0d48b69cd23ff7b7cf9a0739a1961a63ce487
gerrymoeis <gerrymoeis@gmail.com>
2025-12-20 18:37:35 +0700

    944

---
+++ 944_delete_columns_to_make_sorted/script.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+    let deleted = 0;
+    for (let i=0; i < strs[0].length; ++i) {
+        for (let j=1; j < strs.length; ++j) {
+            if (strs[j][i] < strs[j-1][i]) {
+                deleted++;
+                break;
+            }
+        }
+    }
+    return deleted;
+};
